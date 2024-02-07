0d583766c09e09540ebcdcba00f1549eeea5c887
Sagar Jha <sagarjha0511@gmail.com>
2024-02-08 00:16:56 +0530

    false

--- JS/coercion.js
+++ JS/coercion.js
@@ -1,3 +1,11 @@
+// for ture how it's work
+
+// let number = 42;
+// let boolean = true;
+// console.log (number + boolean);
+  
+// for fales how it's works 
+
 let number = 42;
-let boolean = true;
+let boolean = false;
 console.log (number + boolean);
